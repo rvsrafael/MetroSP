900363ebb993ac7353f6afc7591faca7f7d77759
logonrm <logonrm@fiap.com.br>
2017-06-26 22:31:03 -0300

    interceptor em retrofit

--- app/src/main/java/com/example/logonrm/metrosp/api/RetrofitClient.java
+++ app/src/main/java/com/example/logonrm/metrosp/api/RetrofitClient.java
@@ -1,5 +1,8 @@
 package com.example.logonrm.metrosp.api;
 
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -11,8 +14,13 @@ public class RetrofitClient {
 
     public static Retrofit getClient(String baseUrl) {
         if (retrofit==null) {
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addNetworkInterceptor(new StethoInterceptor())
+                    .build();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(baseUrl)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
